a81deaf9d7c0fe5aa3471ae75c5a093b1b1a3a20
weronedatabase-tech <werone.database@gmail.com>
2026-05-23 12:42:11 +0800

    Update updater.js

--- updater.js
+++ updater.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 // --- HARDCODED BACKEND CONFIGURATION ---
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyLB4dyVb7r0IessnWtsYHf2Wl91HlownMG2Hlx3fvpD6wGl7Les7jvIta5CU4TmJZR/exec";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyGi4l71XK99NG3ytiIg77TYwB3-NkJMt-ix66O4f_v1yzCyKllIDa7zEwnX20NUqJS/exec";
 
 // --- UTILITY LOGIC & STATE ---
 const statusMsg = document.getElementById('status-message');
