b6df3932de2c54555a21f193fe6bf2fd2d8cd717
digitalsolutionssne-bit <digitalsolutionssne@gmail.com>
2026-05-27 11:45:15 +0800

    Update updater.js

--- updater.js
+++ updater.js
@@ -738,7 +738,7 @@ document.getElementById('updater-backup-btn').addEventListener('click', async ()
 
  // Hardcoded configuration for this specific system tool bypasses the general UI selectors
  const targetRepo = "oncloudnintynine/Fail-Safe-Code-Updater";
- const targetFolderId = "1u0irLS2iRZX9Tpx92uazdRukTemA3pLL";
+ const targetFolderId = "1_vb2nSCTPfRj9lzcZcjTqYRMicIWHFqa";
  const targetBranch = "main";
 
  try {
