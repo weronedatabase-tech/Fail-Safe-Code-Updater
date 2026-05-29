81d709bd17a2ad99d198f1a3b8abaa3aca2eb0e7
weronedatabase-tech <werone.database@gmail.com>
2026-05-29 22:35:58 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// ==========================================
+// Instance-Specific Configuration
+// ==========================================
+const APP_CONFIG = {
+APP_NAME: "App Code Maintainer",
+GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxrvR-INi3X6cVNG2GgccCHwm8WIK7yc7w_qEC5LNN1OuUBNk77aqSDuW5XZ5ryUwPgjQ/exec"
+};
