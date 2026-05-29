df900899eaead08fa776b0cc9a860ffb1864fdcb
weronedatabase-tech <werone.database@gmail.com>
2026-05-29 22:48:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,8 @@
 // ==========================================
 const APP_CONFIG = {
 APP_NAME: "App Code Maintainer (MYG)",
-GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyGi4l71XK99NG3ytiIg77TYwB3-NkJMt-ix66O4f_v1yzCyKllIDa7zEwnX20NUqJS/exec"
+GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyGi4l71XK99NG3ytiIg77TYwB3-NkJMt-ix66O4f_v1yzCyKllIDa7zEwnX20NUqJS/exec",
+TARGET_REPO: "weronedatabase-tech/Fail-Safe-Code-Updater",
+TARGET_FOLDER_ID: "1u0irLS2iRZX9Tpx92uazdRukTemA3pLL",
+TARGET_BRANCH: "main"
 };
