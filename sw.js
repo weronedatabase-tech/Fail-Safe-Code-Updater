052bc7e3285eeab26337557e90fe11bac923e322
weronedatabase-tech <werone.database@gmail.com>
2026-06-07 08:44:40 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
-const CACHE_NAME = 'fail-safe-updater-v1.1.1';
-
+const CACHE_NAME = 'fail-safe-updater-v1.1.3';
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
+self.skipWaiting();
 });
-
 self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+e.waitUntil(clients.claim());
 });
